9142522dae8d05aedc02dc1a9e88d325cc2cb671
Kris <kristhecanadian101@gmail.com>
2023-04-02 16:42:22 -0400

    done

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import os
 output_dir = "output/"
 
 
-def create_slider_and_label(master, text, variable, command, from_, to):
+def create_slider_and_label(master, text, variable, command, from_, to, number_of_steps: int | None = None):
     label = customtkinter.CTkLabel(master=master, text=text, justify=customtkinter.LEFT)
     label.pack(pady=10, padx=10)
-    slider = customtkinter.CTkSlider(master=master, variable=variable, command=command, from_=from_, to=to)
+    slider = customtkinter.CTkSlider(master=master, variable=variable, command=command, from_=from_, to=to,
+                                     number_of_steps=number_of_steps)
     slider.pack(pady=10, padx=10)
-    slider.set(0.5)
+    slider.set(variable.get())
     return label, slider
 
 
@@ -56,32 +57,73 @@ def segment_callback(value):
     renderType.set(value)
 
 
-def render_step():
-    # k_t
-    # ---------------------------
-    # jLs^3+(jR+bL)s^2+(bR + k_ek_t)s
-
+def render_type(graph_type: str = "Step"):
     denominator, numerator = calculating_g()
     z = var_z.get()
     p = var_p.get()
     k = var_k.get()
 
-    octave_code = "pkg load control\n" \
-                    "clf;\n" \
-                    "s = tf('s');\n" \
-                    "g = " + numerator + "/(" + denominator + ");\n" \
-                    "g_ctrl = zpk(" + str(z) + ", " + str(p) + ", " + str(k) + ");\n" \
-                    "controller = g * g_ctrl / (1 + g * g_ctrl);\n" \
-                    "step(controller);\n" \
-                    "xlabel('Time (s)');\n" \
-                    "ylabel('Amplitude');\n" \
-                    "print -dsvg step.svg\n" \
+    file_name = graph_type.lower() + ".png"
 
+    octave_code = "pkg load control\n" \
+                  "clf;\n" \
+                  "s = tf('s');\n" \
+                  "g = " + numerator + "/(" + denominator + ");\n" \
+                                                            "g_ctrl = zpk(" + str(z) + ", " + str(p) + ", " + str(
+        k) + ");\n" \
+             "controller = g * g_ctrl / (1 + g * g_ctrl);\n" \
+             "" + graph_type.lower() + "(controller);\n" \
+                                       "xlabel('Time (s)');\n" \
+                                       "ylabel('Amplitude');\n" \
+                                       "print -dpng " + file_name + "\n"
+
+    execute_octave_code(octave_code, file_name)
+    display_image(file_name)
+
+
+def create_error_window(message: str):
+    error_window = tk.Toplevel()
+    error_window.title("Error")
+    error_window.geometry("300x300")
+    error_window.resizable(False, False)
+    error_window.configure(bg="#2c2f33")
+
+    error_label = tk.Label(error_window,
+                           text=message,
+                           bg="#2c2f33", fg="white")
+    error_label.pack(pady=10, padx=10)
+
+    error_window.mainloop()
+
+
+def display_image(file_name):
+    # display image in new window
+    image_window = tk.Toplevel()
+    image_window.title(file_name)
+    image_window.geometry("1000x700")
+    image_window.configure(bg="#2c2f33")
+
+    # get current directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    image = tk.PhotoImage(file=os.path.join(current_dir + "/" + output_dir, file_name))
+    image_label = tk.Label(image_window, image=image)
+    image_label.image = image
+    image_label.pack(pady=10, padx=10)
+
+    image_window.mainloop()
+
+
+def execute_octave_code(octave_code: str, file_name: str):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     os.chdir(output_dir)
-    output = subprocess.check_output(['octave', '-q', '--eval', octave_code])
+    try:
+        output = subprocess.check_output(['octave', '-q', '--eval', octave_code])
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        create_error_window(output.decode("utf-8"))
     os.chdir("..")
 
     # check if output finished successfully
@@ -89,8 +131,7 @@ def render_step():
         print("Error while running octave, please make sure octave is installed and in your PATH variable.")
         return
 
-    print("File saved to:", os.path.join(output_dir, "step.svg"))
-    subprocess.Popen(['open', os.path.join(output_dir, "step.svg")])
+    print("File saved to:", os.path.join(output_dir, file_name))
 
 
 def calculating_g():
@@ -108,23 +149,59 @@ def calculating_g():
     return denominator, numerator
 
 
-def render_impulse():
-    pass
-
-
 def render_root_locus():
-    pass
+    denominator, numerator = calculating_g()
+    z = var_z.get()
+    p = var_p.get()
+    k = var_k.get()
+
+    file_name = "root_locus.png"
+
+    octave_code = "pkg load control\n" \
+                  "clf;\n" \
+                  "s = tf('s');\n" \
+                  "g = " + numerator + "/(" + denominator + ");\n" \
+                                                            "g_ctrl = zpk(" + str(z) + ", " + str(p) + ", " + str(
+        k) + ");\n" \
+             "controller = g * g_ctrl / (1 + g * g_ctrl);\n" \
+             "rlocus(controller);\n" \
+             "xlabel('Real');\n" \
+             "ylabel('Imaginary');\n" \
+             "print -dpng " + file_name + "\n"
+
+    execute_octave_code(octave_code, file_name)
+    display_image(file_name)
 
 
 def render_poles():
-    pass
+    denominator, numerator = calculating_g()
+    z = var_z.get()
+    p = var_p.get()
+    k = var_k.get()
+
+    file_name = "poles.png"
+
+    octave_code = "pkg load control\n" \
+                  "clf;\n" \
+                  "s = tf('s');\n" \
+                  "g = " + numerator + "/(" + denominator + ");\n" \
+                                                            "g_ctrl = zpk(" + str(z) + ", " + str(p) + ", " + str(
+        k) + ");\n" \
+             "controller = g * g_ctrl / (1 + g * g_ctrl);\n" \
+             "pzmap(controller);\n" \
+             "xlabel('Real');\n" \
+             "ylabel('Imaginary');\n" \
+             "print -dpng " + file_name + "\n"
+
+    execute_octave_code(octave_code, file_name)
+    display_image(file_name)
 
 
 def render():
     if renderType.get() == "Step":
-        render_step()
+        render_type("Step")
     elif renderType.get() == "Impulse":
-        render_impulse()
+        render_type("Impulse")
     elif renderType.get() == "Root Locus":
         render_root_locus()
     elif renderType.get() == "Poles":
@@ -185,28 +262,28 @@ label = customtkinter.CTkLabel(master=frame_1, text="Controller Parameters", jus
 label.pack(pady=10, padx=10)
 
 # z parameter
-from_z: float = -10
-to_z: float = 10
+from_z: float = -50
+to_z: float = 50
 var_z = tk.DoubleVar()
-var_z.set(from_z + to_z)
+var_z.set(0)
 label_z, slider_z = create_slider_and_label(frame_1, "z: " + str(var_z.get()), var_z, slider_z_callback, from_z,
                                             to_z)
 
 # p parameter
-from_p: float = -10
-to_p: float = 10
+from_p: float = -50
+to_p: float = 50
 var_p = tk.DoubleVar()
-var_p.set(from_p + to_p)
+var_p.set(0)
 label_p, slider_p = create_slider_and_label(frame_1, "p: " + str(var_p.get()), var_p, slider_p_callback, from_p,
                                             to_p)
 
 # k parameter
-from_k: float = 0
-to_k: float = 100
+from_k: float = 0.01
+to_k: float = 25
 var_k = tk.DoubleVar()
 var_k.set(from_k)
 label_k, slider_k = create_slider_and_label(frame_1, "k: " + str(var_k.get()), var_k, slider_k_callback, from_k,
-                                            to_k)
+                                            to_k, 50 * 10 * 10)
 
 # create a segmented button
 renderType = customtkinter.StringVar()
@@ -215,10 +292,6 @@ segmented_button = customtkinter.CTkSegmentedButton(master=app, values=["Step", 
                                                     command=segment_callback)
 segmented_button.set(renderType.get())
 segmented_button.pack(pady=20, padx=10)
-
-# impulse, step, root locus to render
-# show poles for controller tf = G * G_ctrl / (1 + G * G_ctrl) by applying poles command -> closed loop system
-
 
 # create a button
 button = customtkinter.CTkButton(master=app, text="Render", command=render)
