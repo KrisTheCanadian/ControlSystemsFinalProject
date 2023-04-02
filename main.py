077807422ad4ac6fabe7a8c84037a1d98ccf1e76
Kris <kristhecanadian101@gmail.com>
2023-04-02 15:32:57 -0400

    added step

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def slider3_callback(value):
     print(value)
     label_3.configure(text=f"k_e: {value:.2f}")
 
+
 def slider4_callback(value):
     print(value)
     label_4.configure(text=f"k_t: {value:.2f}")
@@ -50,32 +51,31 @@ def slider_k_callback(value):
     label_k.configure(text=f"k: {value:.2f}")
 
 
-def render():
+def segment_callback(value):
+    print(value)
+    renderType.set(value)
+
+
+def render_step():
     # k_t
     # ---------------------------
     # jLs^3+(jR+bL)s^2+(bR + k_ek_t)s
 
-    r = 0.06727
-    l = 0.001882
-
-    j = var_1.get()
-    b = var_2.get()
-    k_e = var_3.get()
-    k_t = var_4.get()
-
-    coefficient_1 = str(j * l)
-    coefficient_2 = str(j * r + b * l)
-    coefficient_3 = str(b * r + k_e * k_t)
-
-    numerator = str(k_t)
-    denominator = coefficient_1 + "*s^3 + " + coefficient_2 + "*s^2 + " + coefficient_3 + "*s"
+    denominator, numerator = calculating_g()
+    z = var_z.get()
+    p = var_p.get()
+    k = var_k.get()
 
     octave_code = "pkg load control\n" \
-                  "clf;\n" \
-                  "s = tf('s');\n" \
-                  "g = " + numerator + "/(" + denominator + ");\n" \
-                  "step(g);\n" \
-                  "print -dsvg step.svg\n"
+                    "clf;\n" \
+                    "s = tf('s');\n" \
+                    "g = " + numerator + "/(" + denominator + ");\n" \
+                    "g_ctrl = zpk(" + str(z) + ", " + str(p) + ", " + str(k) + ");\n" \
+                    "controller = g * g_ctrl / (1 + g * g_ctrl);\n" \
+                    "step(controller);\n" \
+                    "xlabel('Time (s)');\n" \
+                    "ylabel('Amplitude');\n" \
+                    "print -dsvg step.svg\n" \
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -93,19 +93,58 @@ def render():
     subprocess.Popen(['open', os.path.join(output_dir, "step.svg")])
 
 
+def calculating_g():
+    r = 0.06727
+    l = 0.001882
+    j = var_1.get()
+    b = var_2.get()
+    k_e = var_3.get()
+    k_t = var_4.get()
+    coefficient_1 = str(j * l)
+    coefficient_2 = str(j * r + b * l)
+    coefficient_3 = str(b * r + k_e * k_t)
+    numerator = str(k_t)
+    denominator = coefficient_1 + "*s^3 + " + coefficient_2 + "*s^2 + " + coefficient_3 + "*s"
+    return denominator, numerator
+
+
+def render_impulse():
+    pass
+
+
+def render_root_locus():
+    pass
+
+
+def render_poles():
+    pass
+
+
+def render():
+    if renderType.get() == "Step":
+        render_step()
+    elif renderType.get() == "Impulse":
+        render_impulse()
+    elif renderType.get() == "Root Locus":
+        render_root_locus()
+    elif renderType.get() == "Poles":
+        render_poles()
+    else:
+        print("Unknown render type")
+
+
 customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 app = customtkinter.CTk()
-app.geometry("400x860")
+app.geometry("400x900")
 app.title("Control Systems: Electric Motor")
 
 # make minimal window size
-app.minsize(400, 860)
+app.minsize(400, 900)
 
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=10, fill="both", expand=True)
-
 
 # create a label for the frame
 label = customtkinter.CTkLabel(master=frame_1, text="Motor Parameters", justify=customtkinter.LEFT)
@@ -146,18 +185,18 @@ label = customtkinter.CTkLabel(master=frame_1, text="Controller Parameters", jus
 label.pack(pady=10, padx=10)
 
 # z parameter
-from_z: float = 0
-to_z: float = 100
+from_z: float = -10
+to_z: float = 10
 var_z = tk.DoubleVar()
-var_z.set(from_z)
+var_z.set(from_z + to_z)
 label_z, slider_z = create_slider_and_label(frame_1, "z: " + str(var_z.get()), var_z, slider_z_callback, from_z,
                                             to_z)
 
 # p parameter
-from_p: float = 0
-to_p: float = 100
+from_p: float = -10
+to_p: float = 10
 var_p = tk.DoubleVar()
-var_p.set(from_p)
+var_p.set(from_p + to_p)
 label_p, slider_p = create_slider_and_label(frame_1, "p: " + str(var_p.get()), var_p, slider_p_callback, from_p,
                                             to_p)
 
@@ -169,6 +208,13 @@ var_k.set(from_k)
 label_k, slider_k = create_slider_and_label(frame_1, "k: " + str(var_k.get()), var_k, slider_k_callback, from_k,
                                             to_k)
 
+# create a segmented button
+renderType = customtkinter.StringVar()
+renderType.set("Step")
+segmented_button = customtkinter.CTkSegmentedButton(master=app, values=["Step", "Impulse", "Root Locus", "Poles"],
+                                                    command=segment_callback)
+segmented_button.set(renderType.get())
+segmented_button.pack(pady=20, padx=10)
 
 # impulse, step, root locus to render
 # show poles for controller tf = G * G_ctrl / (1 + G * G_ctrl) by applying poles command -> closed loop system
@@ -177,7 +223,5 @@ label_k, slider_k = create_slider_and_label(frame_1, "k: " + str(var_k.get()), v
 # create a button
 button = customtkinter.CTkButton(master=app, text="Render", command=render)
 button.pack(pady=20, padx=10)
-
-
 
 app.mainloop()
